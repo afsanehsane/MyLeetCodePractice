4cdae4e433a05b45421621d158f7c6cdcf67fba8
Afsaneh Saeidanezhad <107625215+afsanehsane@users.noreply.github.com>
2026-09-06 17:54:54 +0100

    Time: 8 ms (92.12%) | Memory: 21.8 MB (92.83%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,14 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         check = {}
-        output = []
         for item in strs:
             key = "".join(sorted(item))
             if key in check:
                 check[key].append(item)
             else:
                 check[key] = [item]
-        output = list(check.values())
-        return output
+        return (list(check.values()))
 
         
